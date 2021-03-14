79eb7eefbf59e5e35a4658b945111fdf5b9fe618
busraglk <busragelik@gmail.com>
2021-03-14 23:51:10 +0300

    update 14.03.21

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -29,7 +30,10 @@ namespace WebAPI.Controllers
         public IActionResult GetAll()
         {
             //Swagger
-            //Dependecy chain-- bağımlılık zinciri        
+            //Dependecy chain-- bağımlılık zinciri
+
+            Thread.Sleep(1000);
+
             var result = _productService.GetAll();
             if (result.Success)
             {
